91c3b263b698b8410ba3ab695a35b4bcbc4e4f0b
Bruno S. Muniz <eng.brunomuniz@outlook.com>
2023-10-08 17:32:32 +0100

    Fix signup path

--- client/src/views/auth/Signup.js
+++ client/src/views/auth/Signup.js
@@ -24,7 +24,7 @@ const Signup = () => {
             password2: password2
         };
 
-        fetch(process.env.REACT_APP_API_ROOT + '/auth/register/', {
+        fetch(process.env.REACT_APP_API_ROOT + 'auth/register/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
